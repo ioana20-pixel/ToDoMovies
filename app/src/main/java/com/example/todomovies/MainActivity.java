dd0e3dda91aa6e2bd25cde795c177a359de14c74
Cosmina <ioanacarnaru@yahoo.com>
2022-03-28 22:38:25 +0300

    finished splash screen

--- app/src/main/java/com/example/todomovies/MainActivity.java
+++ app/src/main/java/com/example/todomovies/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.todomovies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.todomovies.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //The theme should be changed back to the app theme before the setContentView call
+        setTheme(R.style.Theme_TvSeries);
+
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigatin_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
